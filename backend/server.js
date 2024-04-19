e29e35e7f04cf440bed956433e9feb6c87489081
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-04-19 20:31:21 +0300

    deploy part

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+// deploy
+import path from "path"
+
 import authRoutes from "./routes/auth-routes.js";
 import messageRoutes from "./routes/message-routes.js";
 import userRoutes from "./routes/user-routes.js";
@@ -12,6 +15,10 @@ import { app,server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+// deploy
+const __dirname=path.resolve(); // dirname ye3ni root
+
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -20,6 +27,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// deploy
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 server.listen(PORT, () => {
   connectToMongoDB();
